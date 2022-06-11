362f870960d5334ee91da70ed3690d566d153c1d
zhangkx666 <kunloon@qq.com>
2022-06-12 00:07:14 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="text-align: center">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div style="margin: auto; width: 300px; text-align: center">
+      <a-typography-text v-model:content="editableStr" :editable="{ onCancel: handleCancel, onEnd: handleEnd }" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const editableStr = ref('a-typography-text bug')
+const handleCancel = () => {
+  console.log('handleCancel')
+}
+const handleEnd = () => {
+  console.log('handleEnd')
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
